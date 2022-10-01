c2c325452dddd13b35d3ce5ac86401d2f4102028
sriVishalS <vishalS221202@gmail.com>
2022-10-01 14:00:35 +0530

    All Time Classic Game

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function endGame(draw) {
       if (draw) {
             winningMessageTextElement.innerText = 'Draw!'
       } else {
-            winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+            winningMessageTextElement.innerText = `${circleTurn ? "O" : "X"} Wins!`
       }
       winningMessageElement.classList.add('show')
 }
